65aacec51dcc4165069968196a51296eac67eab4
adty486 <aditya.ay519@gmail.com>
2025-01-30 23:06:50 +0530

    added local origin

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3001', 'http://localhost:3001'] // Allow this specific origin
+  origin: ['http://localhost:3001', 'http://localhost:3000'] // Allow this specific origin
 }));
 
 app.use(bodyParser.json());
